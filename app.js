f527ef1218ba07f64ed1d812c8ba04d0a65f940e
copylolcat <oprichard239@gmail.com>
2019-11-07 18:52:04 -0500

    Update express server setup

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var compass = require('node-compass');
-var socket = require('./socket');
+var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-
-socket.onCreate = function(server) {
-  var socketIndex = require('./routes/sockets/index');
-} 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compass({ mode: 'expanded' }));
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
--- app/socket.js
+++
@@ -1,9 +0,0 @@
-var WebSocket = require('ws');
-var socket = { server: null, onCreate: function(){} };
-
-socket.createServer = function(server) {
-    this.server = new WebSocket.Server({ server });
-    this.onCreate(this.server);
-}
-
-exports = module.exports = socket;
